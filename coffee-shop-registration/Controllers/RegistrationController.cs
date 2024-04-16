f91f7e896cbe81474533b6b06b2163d1b2333f26
Christie018 <christie.sullivan018@gmail.com>
2024-04-16 02:47:29 -0400

    Initial commit

---
+++ coffee-shop-registration/Controllers/RegistrationController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using coffee_shop_registration.Models;
+
+namespace coffee_shop_registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //Temp in-memory store
+        static List<Registration> _registration = new List<Registration>
+        {
+            new Registration {
+                Id = 1,
+                FirstName = "Max",
+                LastName = "Rando",
+                Email = "maxrando@example.com",
+                Password = "P@ssw0rd!" },
+            new Registration {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "janedoe@example.com",
+                Password = "SuperSecrete123!" },
+        };
+
+        //Get Registration
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Registration/Details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var registration = _registration.FirstOrDefault(x => x.Id == id);
+            return View(registration);
+        }
+
+        //GET: Registration/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Registration/ Create
+        [HttpPost]
+        public IActionResult Create(Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                registration.Id = _registration.Max(x => x.Id) + 1;
+                _registration.Add(registration);
+
+                return RedirectToAction("Registration", registration);
+            }
+
+            return View(registration);
+        }
+    }
+}
---
+++ coffee-shop-registration/Models/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace coffee_shop_registration.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "last name cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "Email cannot be longer than 50 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
